c7abd81a7b92ce36fda0a703bed08e9d7248d622
Michael Steininger <steininger@informatik.uni-wuerzburg.de>
2021-06-25 17:06:51 +0200

    Update Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="denseweight",
-    version="0.1.0",
+    version="0.0.1",
     author="Michael Steininger",
     author_email="steininger@informatik.uni-wuerzburg.de",
     description="The imbalanced regression method DenseWeight",
